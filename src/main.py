d39ee758fb06fa3bbdebd254fcced7056c338269
Jeremy Tran <jtran0321@gmail.com>
2026-02-23 10:49:58 -0800

    Remove The Muse collector

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from collectors.remoteok_collector import run_remoteok
 from collectors.adzuna_collector import run_adzuna
+from collectors.jobicy_collector import run_jobicy
+from collectors.jsearch_collector import run_jsearch
 
 def main():
     print("Fetching jobs...")
     run_remoteok()
     run_adzuna()
+    run_jobicy()
+    run_jsearch()
     print("Data pushed to Kafka successfully!")
 
 if __name__ == "__main__":
